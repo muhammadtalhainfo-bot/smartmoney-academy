1527dbf251642e4fa4b35476a605a77dd3327454
APPLE ONE <appleone@Muhammad.local>
2026-03-26 00:51:30 +0500

    fix: use org auth key for OneSignal

--- app/api/notify/route.js
+++ app/api/notify/route.js
@@ -5,7 +5,7 @@ export async function POST(req) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Key os_v2_app_oci7h4am6ffpvfmhbqyebnjay65onaba7tcew64gvgyt57zjopmjf6zmoaycmaer7w7ydvs4igz7oehgdjomdq5tzyj23z2t63on2va',
+      'Authorization': 'Key os_v2_org_bnsdrgaq7nem5ad6bpkpyxkoroitwrfmvlnuromudg7isq7qnvrl3hxl7i3nsesbetjkrjiqxlqckt7rzudverztfelch2ci3bypzda',
     },
     body: JSON.stringify({
       app_id: '7091f3f0-0cf1-4afa-9587-0c3040b520c7',
