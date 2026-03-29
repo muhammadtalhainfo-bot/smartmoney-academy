281f69097ce554050bf98003baec26336b08c94b
APPLE ONE <appleone@Muhammad.local>
2026-03-29 16:46:35 +0500

    feat: ICT 2022 Mentorship page, fix courses build

--- app/courses/page.js
+++ app/courses/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { createClient } from '@/lib/supabase';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Navbar from '@/app/components/Navbar';
 import Footer from '@/app/components/Footer';
